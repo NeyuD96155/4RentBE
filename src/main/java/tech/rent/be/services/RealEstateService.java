57ec872a43c98751ec95bfe8bf59b2548e753cff
LVQT <levietquocthinh@gmail.com>
2024-01-24 12:00:00 +0700

    add service to show admin

--- src/main/java/tech/rent/be/services/RealEstateService.java
+++ src/main/java/tech/rent/be/services/RealEstateService.java
@@ -126,6 +126,8 @@ public class RealEstateService {
             realEstateDTO.setPrice(realEstate.getPrice());
             System.out.println(realEstate.getLocation().getLocation());
             System.out.println(realEstate.getCategory().getCategoryname());
+            realEstateDTO.setLocation(realEstate.getLocation().getLocation());
+            realEstateDTO.setCategory(realEstate.getCategory().getCategoryname());
             realEstateDTO.setCategoryId(realEstate.getCategory().getId());
             realEstateDTO.setLocationId(realEstate.getLocation().getId());
 
@@ -146,11 +148,48 @@ public class RealEstateService {
         return estateDTOList;
     }
 
-}
-
     public List<RealEstateDTO> getAllRealEstateToAdmin() {
         List<RealEstate> estateListA = realEstateRepository.findAll();
         return convertToDTOtoAdmin(estateListA);
+    }
+
+    public List<RealEstateDTO> convertToDTOtoAdmin(List<RealEstate> estateList) {
+        List<RealEstateDTO> estateDTOList = new ArrayList<>();
+
+        for (RealEstate realEstate : estateList) {
+            RealEstateDTO realEstateDTO = new RealEstateDTO();
+            // Map
+            realEstateDTO.setId(realEstate.getId());
+            realEstateDTO.setTitle(realEstate.getTitle());
+            realEstateDTO.setDescription(realEstate.getDescription());
+            // realEstateDTO.setDate(realEstate.getDate());
+            realEstateDTO.setEstateStatus(realEstate.getEstateStatus());
+            realEstateDTO.setAmount(realEstate.getAmount());
+            realEstateDTO.setCheckIn(realEstate.getCheckIn());
+            realEstateDTO.setCheckOut(realEstate.getCheckOut());
+            realEstateDTO.setPrice(realEstate.getPrice());
+            System.out.println(realEstate.getLocation().getLocation());
+            System.out.println(realEstate.getCategory().getCategoryname());
+            realEstateDTO.setLocation(realEstate.getLocation().getLocation());
+            realEstateDTO.setCategory(realEstate.getCategory().getCategoryname());
+            realEstateDTO.setCategoryId(realEstate.getCategory().getId());
+            realEstateDTO.setLocationId(realEstate.getLocation().getId());
+
+            estateDTOList.add(realEstateDTO);
+
+            List<ResourceDTO> resourceDTOS = new ArrayList<>();
+            // ResourceDTO => Resource
+            for (Resource resource : realEstate.getResource()) {
+                ResourceDTO resourceDTO = new ResourceDTO();
+                // resourceDTO.setId(realEstate.getId());
+                resourceDTO.setResourceType(resource.getResourceType());
+                resourceDTO.setUrl(resource.getUrl());
+                resourceDTOS.add(resourceDTO);
+            }
+            realEstateDTO.setResources(resourceDTOS);
+
+        }
+        return estateDTOList;
     }
 
 }
