ceedb879c13834a75971b1f253ebfa1fdad5af5f
LVQT <levietquocthinh@gmail.com>
2024-01-25 12:00:00 +0700

    added more location logic

--- src/main/java/tech/rent/be/services/LocationService.java
+++ src/main/java/tech/rent/be/services/LocationService.java
@@ -20,4 +20,17 @@ public class LocationService {
         return locationRepository.save(location);
     }
 
+    public List<LocationDTO> getAllLocation() {
+        List<Location> locations = locationRepository.findAll();
+        return locations.stream()
+                .map(this::convertToLocationDTO)
+                .collect(Collectors.toList());
+    }
+
+    private LocationDTO convertToLocationDTO(Location location) {
+        LocationDTO locationDTO = new LocationDTO();
+        locationDTO.setId(location.getId());
+        locationDTO.setLocation(location.getLocation());
+        return locationDTO;
+    }
 }
