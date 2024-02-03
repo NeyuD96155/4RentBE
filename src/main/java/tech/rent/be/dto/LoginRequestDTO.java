8a4db0a7f79a818c3e40c92f9316160edaac6380
LVQT <levietquocthinh@gmail.com>
2024-02-03 23:59:58 +0700

    real estate

---
+++ src/main/java/tech/rent/be/dto/LoginRequestDTO.java
@@ -0,0 +1,14 @@
+package tech.rent.be.dto;
+
+public class LoginRequestDTO {
+    String username;
+    String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
