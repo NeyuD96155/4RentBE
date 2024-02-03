9d8ec8f971745723d918e9a115a5269fb17ffaeb
LVQT <levietquocthinh@gmail.com>
2024-02-03 23:51:16 +0700

    post

---
+++ src/main/java/tech/rent/be/dto/PostRequestDTO.java
@@ -0,0 +1,55 @@
+package tech.rent.be.dto;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class PostRequestDTO {
+    String content;
+    String title;
+    Long Price;
+    Date PostDate;
+
+    Long userId;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Long getPrice() {
+        return Price;
+    }
+
+    public void setPrice(Long price) {
+        Price = price;
+    }
+
+    public Date getPostDate() {
+        return PostDate;
+    }
+
+    public void setPostDate(Date postDate) {
+        PostDate = postDate;
+    }
+}
