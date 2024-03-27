1cf7b2c11e3f673cde4730dd3fbf336970f4c99f
LVQT <levietquocthinh@gmail.com>
2024-03-28 00:59:03 +0700

    added LoginResponseDTO for login

---
+++ src/main/java/tech/rent/be/dto/LoginResponseDTO.java
@@ -0,0 +1,53 @@
+package tech.rent.be.dto;
+
+
+import tech.rent.be.enums.Role;
+
+public class LoginResponseDTO {
+    long id;
+    String username;
+    String fullName;
+    String token;
+
+    Role role;
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
