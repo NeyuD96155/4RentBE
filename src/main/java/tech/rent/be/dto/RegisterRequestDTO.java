cabbd6433720b89d58fb5997724ecf0e2e1feae5
LVQT <levietquocthinh@gmail.com>
2024-03-28 00:47:03 +0700

    config RegisterAPI

---
+++ src/main/java/tech/rent/be/dto/RegisterRequestDTO.java
@@ -0,0 +1,91 @@
+package tech.rent.be.dto;
+
+import lombok.Data;
+import tech.rent.be.enums.Role;
+
+import java.util.Date;
+
+@Data
+public class RegisterRequestDTO {
+     String username;
+    String password;
+    String email;
+     Role role;
+     String fullname;
+   String dateOfBirth;
+   String gender;
+    String phoneNumber;
+   String address;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
