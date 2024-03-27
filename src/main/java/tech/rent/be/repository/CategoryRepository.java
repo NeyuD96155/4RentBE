b824ced3cec8a606deffb27ac3849044bf5f6c11
LVQT <levietquocthinh@gmail.com>
2024-03-28 03:10:11 +0700

    fix category and location repository to create new estate

--- src/main/java/tech/rent/be/repository/CategoryRepository.java
+++ src/main/java/tech/rent/be/repository/CategoryRepository.java
@@ -3,6 +3,6 @@ package tech.rent.be.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import tech.rent.be.entity.Category;
 
-public interface CategoryRepository  extends JpaRepository<Category, Long> {
+public interface CategoryRepository extends JpaRepository<Category, Long> {
     Category findCategoryById(Long id);
 }
--- src/main/java/tech/rent/be/repository/LocationRepository.java
+++ src/main/java/tech/rent/be/repository/LocationRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import tech.rent.be.entity.Category;
 import tech.rent.be.entity.Location;
 
-public interface LocationRepository  extends JpaRepository<Location, Long> {
+public interface LocationRepository extends JpaRepository<Location, Long> {
     Location findLocationById(Long id);
 }
