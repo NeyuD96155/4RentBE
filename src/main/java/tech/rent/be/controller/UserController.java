7db2c73578b7f257dc6ef08ba0ebb7271a808a20
LVQT <levietquocthinh@gmail.com>
2024-03-28 02:31:51 +0700

    added UserController for user controll

---
+++ src/main/java/tech/rent/be/controller/UserController.java
@@ -0,0 +1,23 @@
+package tech.rent.be.controller;
+
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import tech.rent.be.dto.UserDTO;
+import tech.rent.be.services.UserService;
+import tech.rent.be.utils.AccountUtils;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@SecurityRequirement(name = "api")
+@CrossOrigin("*")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+}
