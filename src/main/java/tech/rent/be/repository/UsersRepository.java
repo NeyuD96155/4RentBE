2127800e23b5706af08fdf1ea0dec5617a4ffde0
LVQT <levietquocthinh@gmail.com>
2024-03-28 02:40:34 +0700

    added findUserbyId

--- src/main/java/tech/rent/be/repository/UsersRepository.java
+++ src/main/java/tech/rent/be/repository/UsersRepository.java
@@ -2,11 +2,14 @@ package tech.rent.be.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import tech.rent.be.entity.Users;
+import tech.rent.be.enums.Role;
 
 import java.util.Optional;
 
-public interface UsersRepository extends JpaRepository<Users,Long> {
+public interface UsersRepository extends JpaRepository<Users, Long> {
     Optional<Users> findByUsername(String username);
 
     Users findUsersById(Long id);
+
+    Users findUsersByRole(Role role);
 }
