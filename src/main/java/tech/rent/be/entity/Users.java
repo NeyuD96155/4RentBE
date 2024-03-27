4f938f4fc2f920beae779068b9871fec2dbfbe22
LVQT <levietquocthinh@gmail.com>
2024-03-28 02:49:07 +0700

    fix entity user to update

--- src/main/java/tech/rent/be/entity/Users.java
+++ src/main/java/tech/rent/be/entity/Users.java
@@ -6,9 +6,11 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import tech.rent.be.enums.AccountStatus;
 import tech.rent.be.enums.Role;
 
 import java.util.Collection;
+import java.util.Date;
 import java.util.List;
 
 @Entity
@@ -20,22 +22,38 @@ public class Users implements UserDetails {
     Long id;
     @Enumerated(EnumType.STRING)
     Role role;
+    @Column(unique = true)
     String username;
     String password;
+    @Column(unique = true)
     String email;
+    @Column(columnDefinition = "nvarchar(255)")
+    String fullname;
+    String dateOfBirth;
 
+    String gender;
+    String phoneNumber;
+    @Column(columnDefinition = "nvarchar(255)")
+    String address;
 
-    @OneToMany(mappedBy = "users")
-    List<Post> posts;
-
-    
-    @OneToMany(mappedBy = "users")
-            @JsonIgnore
-    List<RealEstate> estates;
+    AccountStatus status;
 
     @OneToMany(mappedBy = "users")
     @JsonIgnore
+    List<Post> posts;
+
+    @OneToMany(mappedBy = "users")
+    @JsonIgnore
+    List<RealEstate> estates;
+
+    @OneToMany(mappedBy = "users", cascade = CascadeType.ALL)
+    @JsonIgnore
     List<Booking> bookings;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "wallet_id")
+    @JsonIgnore
+    Wallet wallet;
 
     public Long getId() {
         return id;
@@ -100,5 +118,77 @@ public class Users implements UserDetails {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public AccountStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(AccountStatus status) {
+        this.status = status;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
+
+    public List<RealEstate> getEstates() {
+        return estates;
+    }
+
+    public void setEstates(List<RealEstate> estates) {
+        this.estates = estates;
+    }
+
+    public List<Booking> getBookings() {
+        return bookings;
+    }
+
+    public void setBookings(List<Booking> bookings) {
+        this.bookings = bookings;
     }
 }
