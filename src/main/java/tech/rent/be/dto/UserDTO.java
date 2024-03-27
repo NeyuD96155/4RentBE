bd6c3f473b594630d8ac2aa4eab390d99119da96
LVQT <levietquocthinh@gmail.com>
2024-03-28 02:35:56 +0700

    added userDTO

---
+++ src/main/java/tech/rent/be/dto/UserDTO.java
@@ -0,0 +1,80 @@
+package tech.rent.be.dto;
+
+import tech.rent.be.enums.Role;
+
+public class UserDTO {
+    Long id;
+    Role role;
+    String email;
+
+    String fullname;
+    String dateOfBirth;
+
+    String gender;
+    String phoneNumber;
+    String address;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
