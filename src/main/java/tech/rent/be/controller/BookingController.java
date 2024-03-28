c7383653112de517a8d89839c5b3be82364d8970
trandainghia291203@gmail.com <trandainghia291203@gmail.com>
2024-03-28 14:09:56 +0700

    add BookingController

---
+++ src/main/java/tech/rent/be/controller/BookingController.java
@@ -0,0 +1,41 @@
+package tech.rent.be.controller;
+
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import tech.rent.be.dto.BookingRequestDTO;
+import tech.rent.be.dto.BookingResponse;
+import tech.rent.be.dto.PaymentDTO;
+import tech.rent.be.entity.Booking;
+import tech.rent.be.entity.Payment;
+import tech.rent.be.entity.RealEstate;
+import tech.rent.be.services.BookingService;
+import tech.rent.be.services.PaymentService;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin("*")
+@SecurityRequirement(name = "api")
+public class BookingController {
+    @Autowired
+    PaymentService paymentService;
+
+    @Autowired
+    BookingService bookingService;
+
+    @PostMapping("/vn-pay")
+    public ResponseEntity createOrder(@RequestBody BookingRequestDTO bookingRequestDTO) throws Exception {
+        String url = bookingService.getVnPay(bookingRequestDTO);
+        return  ResponseEntity.ok(url);
+    }
+    @GetMapping("/success")
+    public ResponseEntity<Booking> update(@RequestParam long vnp_TxnRef){
+        Booking booking = bookingService.updatePayment(vnp_TxnRef);
+        return ResponseEntity.ok(booking);
+    }
+
+    
+
+}
