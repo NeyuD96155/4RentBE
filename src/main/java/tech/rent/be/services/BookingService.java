b63123677b013df52a3c2e28dbe3d57d27015161
trandainghia291203@gmail.com <trandainghia291203@gmail.com>
2024-03-28 14:56:03 +0700

    fix cancelBooking for policy

--- src/main/java/tech/rent/be/services/BookingService.java
+++ src/main/java/tech/rent/be/services/BookingService.java
@@ -235,6 +235,7 @@ public class BookingService {
             transactions.setValue((float) (booking.getPrice() * 0.7));
             transactionRepository.save(transactions);
 
+            
             Transactions transactions2 = new Transactions();
             transactions2.setFrom(adminWallet);
             transactions2.setTo(memberWallet);
